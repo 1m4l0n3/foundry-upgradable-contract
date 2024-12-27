8dfea7579e4292bbf23c2b52e45efe9a4dba4de9
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-12-28 01:24:56 +0530

    Making BoxV2 as Initializable and OwnableUpgradable

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract BoxV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
 
     function setNumber(uint256 _number) external {
         number = _number;
@@ -14,5 +27,9 @@ contract BoxV2 {
 
     function getVersion() external pure returns(uint256) {
         return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {
+        
     }
 }
